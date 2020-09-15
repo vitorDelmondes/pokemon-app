1903b70c6b2f6718fc7bad1172083c147dce9359
vDelmondes <dev5moneycloud@moneys-Mac-mini.box>
2020-09-15 17:36:27 -0300

    Começo do vue router e assets.

--- pokemon-app/src/main.js
+++ pokemon-app/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import VueRouter from 'vue-router'
+
+// Vue.use(VueRouter)
+
+// const router = new VueRouter()
 
 Vue.config.productionTip = false
+
+// router.map({
+//   '/home': {
+//     component: Home
+//   },
+//   '/componente-b': {
+//     component: ComponentB
+//   },
+// })
 
 new Vue({
   render: h => h(App),
